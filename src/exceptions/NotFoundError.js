45c3ce8c795f1351ed0402e803819ca71ae8e57b
gerryron <gerryron02@gmail.com>
2022-11-09 02:08:15 +0700

    user registration implemented

--- src/exceptions/NotFoundError.js
+++ src/exceptions/NotFoundError.js
@@ -3,7 +3,7 @@ const ClientError = require('./ClientError');
 class NotFoundError extends ClientError {
   constructor(message) {
     super(message, 404);
-    this.name = 'NotFoundError';
+    this.name = `NotFoundError: ${message}`;
   }
 }
 
