6bde7924ddceaa0d462fff7b938018e6257204cd
gerryron <gerryron02@gmail.com>
2022-11-09 12:25:50 +0700

    authentication implemented

--- src/exceptions/NotFoundError.js
+++ src/exceptions/NotFoundError.js
@@ -3,7 +3,7 @@ const ClientError = require('./ClientError');
 class NotFoundError extends ClientError {
   constructor(message) {
     super(message, 404);
-    this.name = `NotFoundError: ${message}`;
+    this.name = 'NotFoundError';
   }
 }
 
